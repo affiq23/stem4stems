9467c8625eeb3b00f44e2aa0c7c5c155cf071d42
Ari S <ari@client-10-228-210-51.tamulink.tamu.edu>
2024-01-27 16:02:38 -0600

    round logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
 import stem_logo from "../public/stem_logo.png";
 import React from "react";
 import Image from "next/image";
+import { createRoot } from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
 
 export default function Home() {
   return (
     <main>
-      <Image src={stem_logo} alt="STEM logo" />
+      <div>
+        <Image
+          src={stem_logo}
+          alt="STEM logo"
+          className="rounded-full"
+          width={500}
+          height={1000}
+        />
+      </div>
     </main>
   );
 }

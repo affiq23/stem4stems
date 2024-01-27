091ad60bc9dcf6f1976c7c2d6df74f895c61d540
affiq23 <affiqem@gmail.com>
2024-01-27 15:32:54 -0600

    content page

---
+++ pages/content.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Content = () => {
+  return (
+    <div>
+      <h1>Content</h1>
+      <p>Content will go here.</p>
+
+      <div>
+
+        <div>
+        <h2>Science</h2>
+        <p>Science is like being a detective of the world. It helps us understand why things happen and how they work. 
+           Kids can have fun doing simple experiments like making a volcano erupt with baking soda and vinegar, fostering curiosity about the world around them. 
+           Understanding science helps them make sense of everyday phenomena.</p>
+        </div>
+        
+        <div>
+        <h2>Technology</h2>
+        <p>Technology is like magic that we create! It's all about using tools and machines to make our lives better. 
+           Kids can enjoy playing educational games on tablets or computers, learning problem-solving skills while having fun. 
+           They can also explore basic coding concepts through games that involve guiding characters through challenges, sparking an interest in technology as a creative tool.</p>
+        </div>
+
+        <div>
+        <h2>Engineering</h2>
+        <p>Engineering is building cool stuff! Whether it's constructing a simple bridge with popsicle sticks or designing a paper airplane, you get to use their imagination to create. 
+           Engineering is like being a superhero inventor. It's not just about building, but also about solving problems. 
+           Kids can have fun building structures with everyday materials, fostering creativity and critical thinking.</p>
+        </div>
+
+        <div>
+        <h2>Math</h2>
+        <p>Math is like a puzzle that helps us solve problems. You can have fun counting their toys, playing with shapes, and solving math puzzles. 
+           Understanding math is like having a superpower that helps them organize and make sense of the world. 
+           It's not just about numbers; it's about patterns, shapes, and even playing strategic games that involve math concepts.</p>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Content;

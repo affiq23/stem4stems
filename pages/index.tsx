1808eccdbcc440488177b4e2b0bc4bf4f8eae2d5
Ari S <ari@client-10-228-210-51.tamulink.tamu.edu>
2024-01-27 15:17:50 -0600

    added3js

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
-import stem_logo from "public/stem_logo.png";
+import stem_logo from "../public/stem_logo.png";
 import React from "react";
 import Image from "next/image";
+import * as THREE from "three";
 
 export default function Home() {
   return (
-    <main>
-      <Image src={stem_logo} alt="STEM logo" />
-    </main>
+    <>
+      <Image src={stem_logo} alt="stem_logo" />;
+    </>
   );
 }

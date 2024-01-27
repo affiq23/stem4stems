c9b35ac7ada561370410a1e700a943cbc88d01bf
Ari S <ari@client-10-228-210-51.tamulink.tamu.edu>
2024-01-27 15:37:57 -0600

    fiber

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,14 @@ export default function Home() {
   return (
     <main>
       <Image src={stem_logo} alt="STEM logo" />
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial />
+        </mesh>
+      </Canvas>
     </main>
   );
 }
-

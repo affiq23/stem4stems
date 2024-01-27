4d5a6b52b20afba7deb810173aa8fa3fd18d592c
Anirudh Nukala <anirudh.nukala@gmail.com>
2024-01-27 14:12:53 -0600

    api key

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
-import stem_logo from "public/stem_logo.png";
+// import stem_logo from "public/stem_logo.jpg";
 import React from "react";
 import Image from "next/image";
+import Quiz from "@/components/quiz";
+
+
 
 export default function Home() {
   return (
     <main>
-      <Image src={stem_logo} alt="STEM logo" />
+      {/* <Image src={stem_logo} alt="STEM logo" /> */}
+      <Quiz />
     </main>
   );
 }
+

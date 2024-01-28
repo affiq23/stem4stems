f05e5bb9609254120bcfe04f051068b98e7fbb0a
affiq23 <affiqem@gmail.com>
2024-01-27 22:47:42 -0600

    edit

--- pages/content.tsx
+++ pages/content.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
+
+
+
+
+
 const Content = () => {
   return (
     <div>
@@ -23,6 +28,7 @@ const Content = () => {
             <br />
               <div className="flex justify-between items-center">
                  <div>
+                
                  <p className="mb-5 text-font-light text-red-300"> 1. Did you know that bubbles always form into a sphere? It's like nature's way of making perfect little balls of soapy fun! You can create your bubble magic by mixing soap with water and blowing gently through a bubble wand.</p>
                  <p className="mb-5 text-font-light text-yellow-300"> 2. Some flowers change color when you put them in water! It's like they're telling us a secret. Kids can experiment by placing white flowers in different colored water and watch as the petals change hues, showing how plants drink water.</p>
                  <p className="mb-5 text-font-light text-sky-300"> 3. Butterflies undergo an incredible transformation called metamorphosis. They start as caterpillars, form a cozy chrysalis, and then emerge as beautiful butterflies. It's like a magical makeover that happens in nature!</p>
@@ -49,7 +55,7 @@ const Content = () => {
               </div>
               <h1 className="mb-2 text-4xl font-bold text-sky-300 underline-h1"><a href="#">Technology</a></h1>
               < br />
-              <p className="mb-5 font-light text-gray-500 dark:text-gray-400">Technology is like magic that we create! It's all about using tools and machines to make our lives better. 
+              <p className="b-5 text-font-light text-red-300">Technology is like magic that we create! It's all about using tools and machines to make our lives better. 
                 Kids can enjoy playing educational games on tablets or computers, learning problem-solving skills while having fun. 
                 They can also explore basic coding concepts through games that involve guiding characters through challenges, sparking an interest in technology as a creative tool.</p>
             <br />
@@ -70,16 +76,21 @@ const Content = () => {
           <br />
 
           <div>
-            <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+            <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-600 dark:border-gray-700">
               <div className="flex justify-between items-center mb-5 text-gray-500">
               </div>
-              <h1 className="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white underline-h1"><a href="#">Engineering</a></h1>
+              <h1 className="mb-2 text-4xl font-bold text-red-300 underline-h1"><a href="#">Engineering</a></h1>
               < br />
-              <p className="mb-5 font-light text-gray-500 dark:text-gray-400">Engineering is building cool stuff! Whether it's constructing a simple bridge with popsicle sticks or designing a paper airplane, you get to use their imagination to create. 
+              <p className="b-5 text-font-light text-white-300">Engineering is building cool stuff! Whether it's constructing a simple bridge with popsicle sticks or designing a paper airplane, you get to use their imagination to create. 
                 Engineering is like being a superhero inventor. It's not just about building, but also about solving problems. 
                 Kids can have fun building structures with everyday materials, fostering creativity and critical thinking.</p>
             <br />
               <div className="flex justify-between items-center">
+                <div>
+                <p className="mb-5 text-yellow-300"> 1. Engineers design airplanes, and you can too! Folding paper into different airplane shapes and testing which one flies the farthest is like being an airplane engineer. Kids can have friendly competitions to see whose plane goes the longest distance.</p>
+                <p className="mb-5 text-sky-300"> 2. Building towers with colorful blocks is not just fun, it's engineering! Kids can experiment with different block arrangements, learning about balance and stability as they create their mini skyscrapers.</p>
+                <p className="mb-5 text-green-300"> 3. Cardboard boxes are like treasure chests for young engineers. They can turn them into forts, cars, or even robots! This activity sparks creativity and teaches kids about the engineering design process.</p>
+                </div>
                   <a href="#" className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
                   Take the Quiz!
                       <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
@@ -91,16 +102,21 @@ const Content = () => {
           <br />
 
            <div>
-            <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+            <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-600 dark:border-gray-700">
               <div className="flex justify-between items-center mb-5 text-gray-500">
               </div>
               <h1 className="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white underline-h1"><a href="#">Math</a></h1>
               < br />
-              <p className="mb-5 font-light text-gray-500 dark:text-gray-400">Math is like a puzzle that helps us solve problems. You can have fun counting their toys, playing with shapes, and solving math puzzles. 
+              <p className="b-5 text-font-light text-sky-300">Math is like a puzzle that helps us solve problems. You can have fun counting their toys, playing with shapes, and solving math puzzles. 
                 Understanding math is like having a superpower that helps them organize and make sense of the world. 
                 It's not just about numbers; it's about patterns, shapes, and even playing strategic games that involve math concepts.</p>
             <br />
               <div className="flex justify-between items-center">
+                <div>
+                    <p className="mb-5 text-green-300"> 1. Counting is everywhere, even in toyland! Kids can practice counting their toys or organizing them into groups. It's like a little math party with your favorite playthings.</p>
+                    <p className="mb-5 text-yellow-300"> 2. Shapes are like puzzle pieces in the world around us. Kids can go on a shape hunt, finding circles in clocks, squares in books, and triangles in pizza slices. It's a shape-tastic adventure!</p>
+                    <p className="mb-5 text-yellow-300"> 3. Turn everyday activities into number stories. For example, if you have three apples and eat one, how many are left? It's like creating your own math adventures with numbers in real life.</p>
+                </div>
                   <a href="#" className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
                   Take the Quiz!
                       <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>

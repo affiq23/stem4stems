02eb54f1646eba627e2da52db7b463260943c329
Anirudh Nukala <anirudh.nukala@gmail.com>
2024-01-27 18:46:03 -0600

    fixed content

--- pages/content.tsx
+++ pages/content.tsx
@@ -4,17 +4,16 @@ const Content = () => {
   return (
     <div>
       <div>
-
       <div>
         <br />
       <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
               <div className="flex justify-between items-center mb-5 text-gray-500">
                   <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                      <svg className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clip-rule="evenodd"></path><path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path></svg>
-                      Article
                   </span>
               </div>
-              <h1 className="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">Science</a></h1>
+              <h1 className="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white underline-h1">
+                <a href="#">Science</a>
+              </h1>
               < br />
               <p className="mb-5 text-font-light text-gray-500 dark:text-gray-400"> Science is like being a super curious explorer! It's about asking cool questions and doing fun experiments to find out how things work.</p>
               <p className="mb-5 text-font-light text-gray-500 dark:text-gray-400"> Imagine you have a magical detective kit, and your mission is to uncover the secrets of the world.
@@ -23,13 +22,8 @@ const Content = () => {
                 So, put on your explorer hat and get ready for a journey of excitement. Science is your ticket to unraveling the mysteries of the world, and every experiment is a step closer to becoming a fantastic detective of the universe!</p>
             <br />
               <div className="flex justify-between items-center">
-                  <div className="flex items-center space-x-4">
-                      <span className="font-medium dark:text-white">
-                          Bonnie Green
-                      </span>
-                  </div>
                   <a href="#" className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
-                      Read more
+                      Take the Quiz!
                       <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                   </a>
               </div>
@@ -42,25 +36,17 @@ const Content = () => {
             <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
               <div className="flex justify-between items-center mb-5 text-gray-500">
                   <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                      <svg className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clip-rule="evenodd"></path><path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path></svg>
-                      Article
                   </span>
               </div>
-              <h1 className="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">Technology</a></h1>
+              <h1 className="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white underline-h1"><a href="#">Technology</a></h1>
               < br />
               <p className="mb-5 font-light text-gray-500 dark:text-gray-400">Technology is like magic that we create! It's all about using tools and machines to make our lives better. 
                 Kids can enjoy playing educational games on tablets or computers, learning problem-solving skills while having fun. 
                 They can also explore basic coding concepts through games that involve guiding characters through challenges, sparking an interest in technology as a creative tool.</p>
             <br />
               <div className="flex justify-between items-center">
-                  <div className="flex items-center space-x-4">
-                      <img className="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png" alt="Bonnie Green avatar" />
-                      <span className="font-medium dark:text-white">
-                          Bonnie Green
-                      </span>
-                  </div>
                   <a href="#" className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
-                      Read more
+                        Take the Quiz!
                       <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                   </a>
               </div>
@@ -72,26 +58,16 @@ const Content = () => {
           <div>
             <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
               <div className="flex justify-between items-center mb-5 text-gray-500">
-                  <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                      <svg className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clip-rule="evenodd"></path><path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path></svg>
-                      Article
-                  </span>
               </div>
-              <h1 className="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">Engineering</a></h1>
+              <h1 className="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white underline-h1"><a href="#">Engineering</a></h1>
               < br />
               <p className="mb-5 font-light text-gray-500 dark:text-gray-400">Engineering is building cool stuff! Whether it's constructing a simple bridge with popsicle sticks or designing a paper airplane, you get to use their imagination to create. 
                 Engineering is like being a superhero inventor. It's not just about building, but also about solving problems. 
                 Kids can have fun building structures with everyday materials, fostering creativity and critical thinking.</p>
             <br />
               <div className="flex justify-between items-center">
-                  <div className="flex items-center space-x-4">
-                      <img className="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png" alt="Bonnie Green avatar" />
-                      <span className="font-medium dark:text-white">
-                          Bonnie Green
-                      </span>
-                  </div>
                   <a href="#" className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
-                      Read more
+                  Take the Quiz!
                       <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                   </a>
               </div>
@@ -103,92 +79,21 @@ const Content = () => {
            <div>
             <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
               <div className="flex justify-between items-center mb-5 text-gray-500">
-                  <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                      <svg className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clip-rule="evenodd"></path><path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path></svg>
-                      Article
-                  </span>
               </div>
-              <h1 className="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">Math</a></h1>
+              <h1 className="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white underline-h1"><a href="#">Math</a></h1>
               < br />
               <p className="mb-5 font-light text-gray-500 dark:text-gray-400">Math is like a puzzle that helps us solve problems. You can have fun counting their toys, playing with shapes, and solving math puzzles. 
                 Understanding math is like having a superpower that helps them organize and make sense of the world. 
                 It's not just about numbers; it's about patterns, shapes, and even playing strategic games that involve math concepts.</p>
             <br />
               <div className="flex justify-between items-center">
-                  <div className="flex items-center space-x-4">
-                      <img className="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png" alt="Bonnie Green avatar" />
-                      <span className="font-medium dark:text-white">
-                          Bonnie Green
-                      </span>
-                  </div>
                   <a href="#" className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
-                      Read more
+                  Take the Quiz!
                       <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                   </a>
               </div>
             </article>   
           </div>            
-
-
-
-
-
-
-
-
-
-
-        <div>
-            <h2>Science</h2>
-            <p>Science is like being a detective of the world. It helps us understand why things happen and how they work. 
-            Kids can have fun doing simple experiments like making a volcano erupt with baking soda and vinegar, fostering curiosity about the world around them. 
-            Understanding science helps them make sense of everyday phenomena.</p>
-        </div>
-        <div>
-            <p>Did you know that bubbles always form into a sphere? It's like nature's way of making perfect little balls of soapy fun! You can create your bubble magic by mixing soap with water and blowing gently through a bubble wand.</p>
-            <p>Some flowers change color when you put them in water! It's like they're telling us a secret. Kids can experiment by placing white flowers in different colored water and watch as the petals change hues, showing how plants drink water.</p>
-            <p>Butterflies undergo an incredible transformation called metamorphosis. They start as caterpillars, form a cozy chrysalis, and then emerge as beautiful butterflies. It's like a magical makeover that happens in nature!</p>
-        </div>
-
-        <div>
-            <h2>Technology</h2>
-            <p>Technology is like magic that we create! It's all about using tools and machines to make our lives better. 
-            Kids can enjoy playing educational games on tablets or computers, learning problem-solving skills while having fun. 
-            They can also explore basic coding concepts through games that involve guiding characters through challenges, sparking an interest in technology as a creative tool.</p>
-        </div>
-        <div>
-            <h3>Fun Facts!</h3>
-            <p>When you touch your tablet screen, it's like giving it an electric high-five! Your fingers carry a little electricity. The screen has magical sensors that feel this touch and quickly start your favorite games or apps. It's like having a secret conversation with your device!</p>
-            <p>Some games use special glasses or your phone camera to bring characters into the real world. It's like magic! Your device blends the virtual and real worlds, making it feel as if dragons and unicorns are right in your living room. Get ready for adventures beyond the screen!</p>
-            <p>There are apps where animals "talk" back to you! It's like having a chat with digital pets. When you speak or tap, the app's clever technology makes animals respond. It's a fun way to learn about sound and communication while having a virtual conversation with cute creatures!</p>
-        </div>
-
-        <div>
-            <h2>Engineering</h2>
-            <p>Engineering is building cool stuff! Whether it's constructing a simple bridge with popsicle sticks or designing a paper airplane, you get to use their imagination to create. 
-            Engineering is like being a superhero inventor. It's not just about building, but also about solving problems. 
-            Kids can have fun building structures with everyday materials, fostering creativity and critical thinking.</p>
-        </div>
-        <div>
-            <h3>Fun Facts!</h3>
-            <p>Engineers design airplanes, and you can too! Folding paper into different airplane shapes and testing which one flies the farthest is like being an airplane engineer. Kids can have friendly competitions to see whose plane goes the longest distance.</p>
-            <p>Building towers with colorful blocks is not just fun, it's engineering! Kids can experiment with different block arrangements, learning about balance and stability as they create their mini skyscrapers.</p>
-            <p>Cardboard boxes are like treasure chests for young engineers. They can turn them into forts, cars, or even robots! This activity sparks creativity and teaches kids about the engineering design process.</p>
-        </div>
-        
-        <div>
-            <h2>Math</h2>
-            <p>Math is like a puzzle that helps us solve problems. You can have fun counting their toys, playing with shapes, and solving math puzzles. 
-            Understanding math is like having a superpower that helps them organize and make sense of the world. 
-            It's not just about numbers; it's about patterns, shapes, and even playing strategic games that involve math concepts.</p>
-        </div>
-        <div>
-            <h3>Fun Facts!</h3>
-            <p>Counting is everywhere, even in toyland! Kids can practice counting their toys or organizing them into groups. It's like a little math party with your favorite playthings.</p>
-            <p>Shapes are like puzzle pieces in the world around us. Kids can go on a shape hunt, finding circles in clocks, squares in books, and triangles in pizza slices. It's a shape-tastic adventure!</p>
-            <p>Turn everyday activities into number stories. For example, if you have three apples and eat one, how many are left? It's like creating your own math adventures with numbers in real life.</p>
-        </div>
-
       </div>
     </div>
   );

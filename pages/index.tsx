5f387501554578737e0fcf1661bb85a86eee5843
Ari S <ari@client-10-228-210-51.tamulink.tamu.edu>
2024-01-27 19:52:08 -0600

    uncommented index

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,50 +38,49 @@ export default function Home() {
   }, []);
 
   return (
-    <Form />
-    // <div className="flex min-h-screen bg-black text-white">
-    //   <aside className="w-60 bg-gray-800 p-5 ml-5 mt-5 mb-5 rounded-lg shadow-lg">
-    //     <div className="flex flex-col items-center">
-    //       <div className="w-52 h-52">
-    //         <Canvas>
-    //           <ambientLight />
-    //           <pointLight position={[10, 10, 10]} />
-    //           <RotatingImage texture={texture} />
-    //         </Canvas>
-    //       </div>
+    <div className="flex min-h-screen bg-black text-white">
+      <aside className="w-60 bg-gray-800 p-5 ml-5 mt-5 mb-5 rounded-lg shadow-lg">
+        <div className="flex flex-col items-center">
+          <div className="w-52 h-52">
+            <Canvas>
+              <ambientLight />
+              <pointLight position={[10, 10, 10]} />
+              <RotatingImage texture={texture} />
+            </Canvas>
+          </div>
 
-    //       <h2 className="text-xl font-semibold my-5">The World of STEM</h2>
-    //       <nav className="flex flex-col gap-4">
-    //         <a href="#" className="hover:text-blue-400">
-    //           Home
-    //         </a>
-    //         <a href="#" className="hover:text-blue-400">
-    //           About STEM
-    //         </a>
-    //         <a href="#" className="hover:text-blue-400">
-    //           Activities
-    //         </a>
-    //         <a href="#" className="hover:text-blue-400">
-    //           Contact
-    //         </a>
-    //       </nav>
-    //     </div>
-    //   </aside>
+          <h2 className="text-xl font-semibold my-5">The World of STEM</h2>
+          <nav className="flex flex-col gap-4">
+            <a href="#" className="hover:text-blue-400">
+              Home
+            </a>
+            <a href="#" className="hover:text-blue-400">
+              About STEM
+            </a>
+            <a href="#" className="hover:text-blue-400">
+              Activities
+            </a>
+            <a href="#" className="hover:text-blue-400">
+              Contact
+            </a>
+          </nav>
+        </div>
+      </aside>
 
-    //   <main className="flex-grow">
-    //     <section className="text-center py-12">
-    //       <span className="flex place-content-center pt-14 text-6xl font-bold text-transparent bg-clip-text ">
-    //         <h1 className="animate-text text-transparent bg-clip-text bg-gradient-to-r from-blue-200 via-teal-500 to-red-200">
-    //           Discover the fun of STEM!
-    //         </h1>
-    //       </span>
+      <main className="flex-grow">
+        <section className="text-center py-12">
+          <span className="flex place-content-center pt-14 text-6xl font-bold text-transparent bg-clip-text ">
+            <h1 className="animate-text text-transparent bg-clip-text bg-gradient-to-r from-blue-200 via-teal-500 to-red-200">
+              Discover the fun of STEM!
+            </h1>
+          </span>
 
-    //       <p className="text-2xl mx-auto leading-relaxed max-w-2xl">
-    //         Explore amazing science facts, cool tech, engineering projects, and
-    //         fun math games!
-    //       </p>
-    //     </section>
-    //   </main>
-    // </div>
+          <p className="text-2xl mx-auto leading-relaxed max-w-2xl">
+            Explore amazing science facts, cool tech, engineering projects, and
+            fun math games!
+          </p>
+        </section>
+      </main>
+    </div>
   );
 }
